2f10eeebceb1e5e87677740833b1f60949ae6d3e
Sighvatur <hvati87@gmail.com>
2013-11-02 02:37:44 +0000

    adding classes

---
+++ js/entities/Class material/Abilities.js
@@ -0,0 +1,37 @@
+// =========
+// ABILITIES
+// =========
+
+"use strict";
+
+/* jshint browser: true, devel: true, globalstrict: true */
+
+/*
+0        1         2         3         4         5         6         7         8
+12345678901234567890123456789012345678901234567890123456789012345678901234567890
+*/
+
+
+// A generic contructor which accepts an arbitrary descriptor object
+function Abil(descr) {
+    for (var property in descr) {
+        this[property] = descr[property];
+    }
+}
+// Initial, inheritable, default values
+
+
+Abil.prototype.update = function () {
+	// TODO: Implement this
+};
+
+Abil.prototype.energy = function () {
+	// TODO: Implement this
+};
+
+Abil.prototype.Spells = function () {
+	// TODO: Implement this
+};
+
+
+
