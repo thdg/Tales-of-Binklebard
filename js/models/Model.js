78bc31d4f62cdea101b7c5308bd290936f3a72ad
thdg <thdg90@gmail.com>
2013-11-11 21:48:20 +0000

    make Model class have everything shared

--- js/models/Model.js
+++ js/models/Model.js
@@ -6,10 +6,64 @@
 
 \************************************************************************/
 
-function Model() {
-
+function Model(descr) {
+    for (var property in descr) {
+        this[property] = descr[property];
+    }
 }
 
-Model.prototype.drawAt(ctx, x, y) {
-    ctx.drawImage(this._activeSprite[this._activeFrame]);
-}
+Model.prototype.update = function(du) {
+
+    var finishedAnimation = this._activeAnimation.update(du);
+    if (this._frozen) this._frozen = !finishedAnimation;
+    else this._activeAnimation = this._animations[this._rotation][this._state];
+};
+
+Model.prototype.drawCentredAt = function(ctx, cx, cy) {
+    this._activeAnimation.drawCentredAt(ctx, cx, cy);
+};
+
+Model.prototype._setState = function(state) {
+    if (this._state === state || this._frozen ) return;
+    this._state = state;
+};
+
+Model.prototype._setRotation = function(rotation) {
+    if (this._rotation === rotation || this._frozen ) return;
+    this._rotation = rotation;
+};
+
+Model.prototype._freezInState = function() {
+    this._activeAnimation = this._animations[this._rotation][this._state];
+    this._frozen = true;
+};
+
+Model.prototype.faceUp = function() {
+    this._setRotation(this.FACE_UP);
+};
+
+Model.prototype.faceDown = function() {
+    this._setRotation(this.FACE_DOWN);
+};
+
+Model.prototype.faceLeft = function() {
+    this._setRotation(this.FACE_LEFT);
+};
+
+Model.prototype.faceRight = function() {
+    this._setRotation(this.FACE_RIGHT);
+};
+
+Model.prototype.halt = function() {
+    this._setState(this.HALT);
+};
+
+Model.prototype.attack = function() {
+    this._setState(this.ATTACKING);
+    this._freezInState();
+};
+
+Model.prototype.walk = function() {
+    this._setState(this.WALKING);
+};
+
