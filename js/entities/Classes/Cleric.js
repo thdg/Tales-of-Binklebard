c895cae1778f8318443fcb1a52d4a275488e52a0
Sighvatur <hvati87@gmail.com>
2013-11-11 23:19:34 +0000

    Breyta foritstexta cleric, character

--- js/entities/Classes/Cleric.js
+++ js/entities/Classes/Cleric.js
@@ -7,10 +7,6 @@
 
 \************************************************************************/
 
-this.mainAtt = this.Wis;
-this.secAtt = this.Str;
-this.weakAtt = this.Dex;
-
 function Cleric(descr) {
     this.setup(descr);
     this.lvlup();
@@ -21,15 +17,8 @@ function Cleric(descr) {
     this.sprite = this.sprite || g_sprites.link;
 }
 
-function updateStats() {
-    this.energy = this.mainAtt * 30;
-    this.armor = this.Dex * 20 + this.MainAtt * 5;
-    this.hp = this.Str * 50;
-    this.damage = this.Str * 5 + this.mainAtt;
-}
-
-Character.prototype.setExp = function (expRew) {
-    this.experience = this.experience + expRew;
+setExp: function (expReward) {
+    this.experience = this.experience + expReward;
     
     if (this.experience >= (lvl * lvl * 1000) + lvl * 2000) {
         this.lvlup();
@@ -37,3 +26,23 @@ Character.prototype.setExp = function (expRew) {
         this.updateStats();
     }
 };
+
+setAtt: function () {
+    this.Wis++;
+    this.Spir++;
+    
+    if(lvl%2 === 0){
+        this.Str++;
+    }
+    if(lvl%3 === 0){
+        this.Dex++;
+    }
+    
+};
+
+updateStats: function () {
+    this.energy = this.Wis * 30;
+    this.armor = this.Dex * 20;
+    this.hp = this.Str * 50;
+    this.damage = this.Str * 5 + this.Wis;
+}
